6a299a64f4cd9d94027cf28fa2507409c5667abc
payel <payel.nandy2580@gmail.com>
2022-07-10 11:19:07 -0400

    greet

---
+++ greet.py
@@ -0,0 +1,24 @@
+# print("Payel")
+#
+# print("Hello Payel!")
+
+
+def name():
+    print("Payel Banerjee")
+
+
+name()
+
+
+def input_name():
+    a = input("Please input your name ")
+    return a
+def greet(name_greet):
+    print("Hello " + name_greet)
+
+greet(input_name())
+
+
+
+
+
